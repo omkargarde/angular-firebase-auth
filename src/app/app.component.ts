0fc8f04c1ac6b0b237d271ebf139f7f352a3cb2a
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-04-30 07:22:28 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-firebase-auth';
+  http = inject(HttpClient);
+
+  logout(): void {
+    console.log('logout');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: LoginComponent,
+    // loadChildren: () =>
+    //   import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'login',
+    component: RegisterComponent,
+    // loadChildren: () =>
+    //   import('./register/register.component').then((m) => m.RegisterComponent),
+  },
+];
